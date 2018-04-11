55b208f77188d7b87c1cfe82b06c4ac856112084
winniekao <winniekao@gmail.com>
2018-04-11 16:19:35 +0800

    change some var

--- w2v_wiki.py
+++ w2v_wiki.py
@@ -12,9 +12,9 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 #logging.basicConfig(format='%(asctime)s : %(levelmame)s : %(message)s', level=logging.INFO )
 
-sentences = word2vec.LineSentence("./wiki_seg.txt")
-model = word2vec.Word2Vec(sentences, size=250)
+sentences = word2vec.LineSentence("./wiki_seg_zhTW_search.txt")
+model = word2vec.Word2Vec(sentences, size=250, sg=1, min_alpha=0.00001)
 
-model.save("word2vec_wiki.model")
+model.save("word2vec_wiki_zhTW_search.model")
 
 
